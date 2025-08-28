46fcfbd9b656b8c058ce4c073fbcb8e8cb09d140
Pete Benbow <72094182+pebenbow@users.noreply.github.com>
2025-08-28 07:11:53 -0400

    Create ex_02_select_statements.sql

---
+++ ex_02_select_statements.sql
@@ -0,0 +1,151 @@
+/*
+SELECT statements
+DAT-153 Database Programming
+*/
+
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+
+/*
+Exercise 1
+
+Locate all countries with populations greater than 68 million people. Remember
+to sort your results in ascending alphabetical order, first by region, then by
+country. At the end, limit the results to just 10 rows, and include only the
+following columns:
+
+-   region
+-   country
+-   capital
+-   population
+-   area
+*/
+
+
+
+--------------------------------------------------------------------------------
+
+/*
+Exercise 2
+
+How many countries have fewer than 1 million inhabitants? This time, instead of
+returning columns, use `COUNT(*)` function inside of your `SELECT` clause to
+return a simple row count.
+*/
+
+
+
+--------------------------------------------------------------------------------
+
+/*
+Exercise 3
+
+Write a SQL query to find the capital city of Liberia. The capital city should
+be the only column returned by your query.
+*/
+
+
+
+--------------------------------------------------------------------------------
+
+/*
+Exercise 4
+
+Write a SQL query to find all countries where their region contains the word
+"Asia", sorted by region then country. Sort your results in ascending order by
+region and country, and return only these columns:
+
+-   region
+-   country
+-   population
+-   capital
+*/
+
+
+
+--------------------------------------------------------------------------------
+
+/*
+Exercise 5
+
+Write a SQL query to find all South Asian countries OR countries with populations
+greater than 100 million people. Sort your results in ascending order by region
+and country, and return only these columns:
+
+-   region
+-   country
+-   population
+-   capital
+
+*/
+
+
+
+--------------------------------------------------------------------------------
+
+/*
+Exercise 6
+
+Write a SQL query to find all countries in the Middle East & North Africa whose
+land area consists of between 500K and 2 million square kilometers. Sort your
+results is descending order by area, and return only these columns:
+
+-   country
+-   area
+-   region
+
+*/
+
+
+
+--------------------------------------------------------------------------------
+
+/*
+Exercise 7
+
+Find all countries located in either "East Asia & Pacific" or "South Asia," and
+that have either a population of more than 100 million or an area greater than
+1 million square kilometers. Sort your results is descending order by area, and
+return only these columns:
+
+-   country
+-   region
+-   population
+-   area
+
+This question requires you to use both the `AND` and `OR` operators, so read the
+prompt carefully and consider the order of operations here.
+*/
+
+
+
+--------------------------------------------------------------------------------
+
+/*
+Exercise 8
+
+Use `SELECT DISTINCT` to return an alphabetically sorted list of all regions in
+the countries table.
+*/
+
+
+
+--------------------------------------------------------------------------------
+
+/*
+Exercise 9
+
+Use the IN operator to find all countries situated in either North America or
+Sub-Saharan Africa. Sort your results is descending order by area, and return
+only these columns:
+
+-   country
+-   area
+-   region
+*/
+
+
+
+--------------------------------------------------------------------------------
+
